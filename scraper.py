72f6007a8ff00bab96807ff67c303d400a67cdf7
rpnugroho <nugroho.rizkyp@gmail.com>
2021-01-24 19:12:53 +0700

    update

--- scraper.py
+++ scraper.py
@@ -111,8 +111,8 @@ def update_dataset(new_df):
 if __name__ == "__main__":
 	api = twitter_api()
 	search_words = '#vaksin OR #vaksinasi -filter:retweets'
-	date_since = '2021-01-22'
-	date_until = '2021-01-23'
+	date_since = '2021-01-23'
+	date_until = '2021-01-24'
 	print('Search {0} since {1} until {2}'.format(search_words, date_since, date_until))
 	# print('DATE {}'.format(date_since))
 	tweet_list = create_cursor(api,
